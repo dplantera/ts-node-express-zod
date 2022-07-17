736dd5a63c4bf672e2b504a04597af35643837fe
danie <daniele.plantera@outlook.de>
2022-07-17 19:41:01 +0200

    node express with custom zod validation

--- src/server/controller/UsersController.ts
+++ src/server/controller/UsersController.ts
@@ -1,25 +1,26 @@
 import { App, Controller } from "../ApplicationServer";
-import { NextFunction, Request, Response } from "express";
-import { WithSchemaValidation } from "../middlewares/withSchemaValidation.middleware";
-import { ResultResponse, ValidatedRequest } from "../middlewares/zod-express-validation.middleware";
+import { Middleware, SchemaValidationMiddleware } from "../middlewares/SchemaValidation.middleware";
 import { schemaUsersPost, schemaUsersPostResponse, User, UserResult } from "../api/schemas";
 
 
 export class UsersController implements Controller {
   register(app: App) {
     app.post("/api/users",
-      WithSchemaValidation.middleware(this.helloWorld.bind(this), {
+      SchemaValidationMiddleware.create(this.createUser.bind(this), {
         requestSchema: schemaUsersPost,
         responseSchema: schemaUsersPostResponse
       })
     )
   }
 
-  helloWorld(req: ValidatedRequest<User>, res: ResultResponse<UserResult>, next: NextFunction) {
+  createUser(...params: Parameters<Middleware<User, UserResult>>) {
+    const [req, res, next] = params;
     const user = req.body;
-    console.log(user)
-    user.id = "new-user-1";
-    res.result = user;
+    res.result = {
+      ...user,
+      someField: "will-be-removed",
+      id: "new-user-1"
+    };
     next()
   }
 }
